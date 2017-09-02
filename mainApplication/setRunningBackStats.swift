09aad363e75f7b4c1cca5b112efb3d62f14eb186
avermeulen14 <andreasvermeulen45@gmail.com>
2017-09-02 12:28:28 -0400

    made position classes

--- mainApplication/setRunningBackStats.swift
+++ mainApplication/setRunningBackStats.swift
@@ -6,4 +6,8 @@ func FindNewProability(choosenTeam: String) //pass in the array as well
     //logic to determine what the new probability shoud be
 }
 
-func
+//function that will get the opponent stats. 
+func GetOpponentStats()
+{
+
+}
