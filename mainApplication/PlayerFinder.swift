04d1def1799988def67b093f84e0d1f8282304d6
avermeulen14 <andreasvermeulen45@gmail.com>
2017-09-01 22:02:51 -0400

    created new files for development

---
+++ mainApplication/PlayerFinder.swift
@@ -0,0 +1,23 @@
+import foundation
+
+struct RunningBAckStats
+{
+  var yardsPerCarry: Float
+  var carries: Int //Round the numbers
+  var receptions: Int
+  var touchdowns: Int
+}
+
+playerArray: [String]
+
+//function that will take in an entered player and find that player -- loading his stats.
+func FindPLayer () // find a given player and load there stat array.
+{
+  //loop through all the arrays and get that players set it to the current to use.
+}
+
+//function that will add a plaeyr to the list of players. 
+func AddPlayer ()
+{
+
+}
---
+++ mainApplication/setRunningBackStats.swift
@@ -0,0 +1,9 @@
+import foundation
+
+
+func FindNewProability(choosenTeam: String) //pass in the array as well
+{
+    //logic to determine what the new probability shoud be
+}
+
+func
