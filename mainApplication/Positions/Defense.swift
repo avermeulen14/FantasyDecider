9935a80451693b76cd93709f7aa79c64d51234cf
avermeulen14 <andreasvermeulen45@gmail.com>
2017-09-03 16:04:36 -0400

    finished gets and sets on positions

--- mainApplication/Positions/Defense.swift
+++ mainApplication/Positions/Defense.swift
@@ -1,6 +1,35 @@
 import Foundation
 
+//class for the defense position
 class Defense
 {
-  
+  private var pointsAllowed: Float
+  {
+    get { return pointsAllowed}
+    set { pointsAllowed = newValue}
+  }
+
+  private var pointsScored: Float
+  {
+    get { return pointsScored}
+    set { pointsScored = newValue}
+  }
+
+  private var interceptions: Float
+  {
+    get { return interceptions}
+    set { interceptions = newValue}
+  }
+
+  private var sacks: Float
+  {
+    get { return sacks}
+    set { sacks = newValue}
+  }
+
+  private var teamName: String
+  {
+    get { return teamName}
+    set { teamName = newValue}
+  }
 }
--- mainApplication/Positions/Kicker.swift
+++ mainApplication/Positions/Kicker.swift
@@ -1,6 +1,23 @@
 import Foundation
 
+//class that will support the kicker class
 class Kicker
 {
-  
+  private var fieldGoalsAttempted: Int
+  {
+    get { return fieldGoalsAttempted}
+    set { fieldGoalsAttempted = newValue}
+  }
+
+  private var fieldGoalsMade40Plus: Int
+  {
+    get { return fieldGoalsMade40Plus}
+    set { fieldGoalsMade40Plus = newValue}
+  }
+
+  private var name : String
+  {
+    get { return name}
+    set { name = newValue}
+  }
 }
